9e4582adce5ee4fa4478a497b2d08b15ab752f8f
Kirn Kim <squrkidev@gmail.com>
2025-02-04 12:54:11 -0800

    adding event code

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill("black")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
         
 
 
